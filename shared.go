d7667ffb2f631e5e0040ac19da5e7b5aba4e325b
Kirin Murphy <KirinMurphy@ip-10-22-1-106.us-east-2.compute.internal>
2022-02-03 16:59:40 -0600

    adapting demo to sign up use case

--- shared.go
+++ shared.go
@@ -1,12 +1,17 @@
 package app
 
 // @@@SNIPSTART money-transfer-project-template-go-shared-task-queue
-const TransferMoneyTaskQueue = "TRANSFER_MONEY_TASK_QUEUE"
+const SignUpTaskQueue = "SIGN_UP_TASK_QUEUE"
 // @@@SNIPEND
 
 type TransferDetails struct {
 	Amount      float32
 	FromAccount string
 	ToAccount   string
+	ReferenceID string
+}
+
+type SignUpDetails struct {
+	Email string
 	ReferenceID string
 }
--- workflow_test.go
+++ workflow_test.go
@@ -20,7 +20,6 @@ func Test_Workflow(t *testing.T) {
 		ReferenceID: "1234",
 	}
 	env.OnActivity(Withdraw, mock.Anything, testDetails).Return(nil)
-	env.OnActivity(Deposit, mock.Anything, testDetails).Return(nil)
 	env.ExecuteWorkflow(TransferMoney, testDetails)
 	require.True(t, env.IsWorkflowCompleted())
 	require.NoError(t, env.GetWorkflowError())
